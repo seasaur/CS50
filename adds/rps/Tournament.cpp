785a397995174b082b3aa34ff712b0e8e2a0883e
seasaur <79822665+seasaur@users.noreply.github.com>
2022-03-17 10:24:57 +0000

    Thu, Mar 17, 2022, 10:24 AM +00:00

--- adds/rps/Tournament.cpp
+++ adds/rps/Tournament.cpp
@@ -10,12 +10,22 @@
 #include "FistfullODollars.h"
 #include "PaperDoll.h"
 #include "RandomComputer.h"
+#include "Tournament.h"
 
 Tournament::Tournament() {
 
 }
 
 Player Tournament::* run(std::array<Player *, 8> competitors){
+    Avalanche *player1 = new Avalanche();
+    Bureaucrat *player2 = new Bureaucrat();
+    Toolbox *player3 = new Toolbox();
+    Crescendo *player4 = new Crescendo();
+    PaperDoll *player5 = new PaperDoll();
+    FistfullODollars *player6 = new FistfullODollars();
+    RandomComputer *player7 =  new RandomComputer();
+    Human *player8 = new Human();
+    competitors[8] = {player1,player2,player3,player4,player5,player6,player7,player8};
     for(int i=0;i<5;i++) { //each round
         for int(j=0;j<8;j++) {
             competitors[i]->makeMove();
