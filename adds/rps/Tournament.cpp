e0c4b82b320f8bc0542f731e1474276620a0cf67
seasaur <79822665+seasaur@users.noreply.github.com>
2022-03-18 01:20:37 +0000

    Fri, Mar 18, 2022, 1:20 AM +00:00

--- adds/rps/Tournament.cpp
+++ adds/rps/Tournament.cpp
@@ -18,16 +18,15 @@ Tournament::Tournament() {
 }
 
 Player Tournament::* run(std::array<Player *, 8> competitors){
+    Referee referee;
     char results[5];
     for(int i=0;i<7;i+2) { //matches players 1-2, 3-4, 5-6, 7-8
         for (int j=0;j<5;j++) { //each bracket plays 5 rounds
-            // competitors[i]->Referee();
-            // competitors[i+1]->Referee();
-            
-
+            results[j] = referee.refGame(competitors[i],competitors[i+1]);
         }
-
     }
+
+    
 
 }
 
