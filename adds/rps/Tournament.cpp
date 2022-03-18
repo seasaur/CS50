795aeaaaff93fac552e4ebe1557adb6405e51f04
seasaur <79822665+seasaur@users.noreply.github.com>
2022-03-18 02:20:47 +0000

    Fri, Mar 18, 2022, 2:20 AM +00:00

--- adds/rps/Tournament.cpp
+++ adds/rps/Tournament.cpp
@@ -20,10 +20,10 @@ Tournament::Tournament() {
 Player Tournament::* run(std::array<Player *, 8> competitors){
     Referee referee;
     char results[5];
-    Player * winners[4];
+    Player * winners[4]; //winners array
     int winCount=0; //count for results array
     int loseCount=0;
-    int winnerCount = 0; //winner array
+    int winnerCount = 0; //winner count
 
     for(int i=0;i<7;i+2) { //ROUND 1 matches players 1-2, 3-4, 5-6, 7-8
         for (int j=0;j<5;j++) { //each bracket plays 5 rounds
@@ -48,8 +48,8 @@ Player Tournament::* run(std::array<Player *, 8> competitors){
         loseCount=0;
         results[5] = {0};
     }
-
-    winnerCount={0}; //reset winner array
+    winners[4]={0}; //reset winner array
+    winnerCount = 0;
 
     for(int i=0;i<3;i+2) { //ROUND 2 matches winners 1-2 3-4
         for(int j=0;j<5;j++) {
@@ -75,7 +75,32 @@ Player Tournament::* run(std::array<Player *, 8> competitors){
         results[5] = {0};
     }
 
+    winners[4]={0}; //reset winner array
+    winnerCount = 0;
 
 
+    for(int i=0;i<3;i+2) { //ROUND 2 matches winners 1-2 3-4
+        for(int j=0;j<5;j++) {
+            results[j] = referee.refGame(winners[i],competitors[i+1]);
+        }
+        for(int k=0;k<5;k++) { //check number of wins & losses in terms of player with lower index
+            if(results[k] == 'W') {
+                winCount++;
+            }
+            if(results[k] == 'L') {
+                loseCount++;
+            }
+        }
+        if((winCount>loseCount) || (winCount==loseCount)) { //lower index player win or lose
+            winners[winnerCount] = competitors[i];
+        }
+        else {
+            winners[winnerCount] == competitors[i+1];
+        }
+        winnerCount++;
+        winCount=0; //resetting counters
+        loseCount=0;
+        results[5] = {0};
+    }
 }
 
