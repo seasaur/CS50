d962e87532f4c770d8cbdef6a9efa2d8e8217125
seasaur <79822665+seasaur@users.noreply.github.com>
2022-03-22 01:11:45 +0000

    Tue, Mar 22, 2022, 1:11 AM +00:00

--- wdc/prac3/task3.js
+++ wdc/prac3/task3.js
@@ -3,7 +3,7 @@ function mouseOver(){
     document.getElementById("count").innerText = i++;
 }
 
-function click() {
+function clickFunc() {
     document.getElementsByClassName("post-time")[0].innerHTML = Date();
     var displayText = document.getElementsByTagName("textarea")[0].value;
     document.getElementsByClassName("post-content")[0].innerHTML = displayText;
