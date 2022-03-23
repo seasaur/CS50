5ecb1577f417d63bdd31b6a700f0ae4a1ffb3bb4
seasaur <79822665+seasaur@users.noreply.github.com>
2022-03-23 01:45:31 +0000

    Wed, Mar 23, 2022, 1:45 AM +00:00

--- wdc/prac3/task3.js
+++ wdc/prac3/task3.js
@@ -78,14 +78,14 @@ function postColor(postText) {
     postText.style.color= selectedColor;
 }
 
-function postStyle(postText) {
-    var checkBox = document.querySelectorAll('input[name="style"]');
-    var textStyle;
-    for(var i=0, n=checkbox.length;i<n;i++) {
-        if (checkBox[i].checked == true){
-            textStyle = checkBox[i].value;
-            break;
-        }
-    }
-    textStyle.style.fontStyle = textStyle;
-}
+// function postStyle(postText) {
+//     var checkBox = document.querySelectorAll('input[name="style"]');
+//     var textStyle;
+//     for(var i=0, n=checkbox.length;i<n;i++) {
+//         if (checkBox[i].checked == true){
+//             textStyle = checkBox[i].value;
+//             break;
+//         }
+//     }
+//     textStyle.style.fontStyle = textStyle;
+// }
