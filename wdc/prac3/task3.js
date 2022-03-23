e7f1637a891050fa18cda7eb42823543c5c66085
seasaur <79822665+seasaur@users.noreply.github.com>
2022-03-23 03:23:08 +0000

    Wed, Mar 23, 2022, 3:23 AM +00:00

--- wdc/prac3/task3.js
+++ wdc/prac3/task3.js
@@ -79,3 +79,19 @@ function postStyle(postText) {
         }
     }
 }
+
+function postRange(postNumber) {
+    var checkBox = document.querySelectorAll('input[name="style"]');
+    var textStyle;
+    for(var i=0, n=checkBox.length;i<n;i++) {
+        if (checkBox[i].checked == true){
+            textStyle = checkBox[i].value;
+            if(textStyle=="italic") {
+                postText.style.fontStyle = textStyle;
+            }
+            if(textStyle=="bold") {
+                postText.style.fontWeight = textStyle;
+            }
+        }
+    }
+}
