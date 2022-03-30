6bd4337a826c8d26dee157257b8d9f83d95303fa
seasaur <79822665+seasaur@users.noreply.github.com>
2022-03-30 03:54:49 +0000

    Wed, Mar 30, 2022, 3:54 AM +00:00

--- wdc/prac4/routes/index.js
+++ wdc/prac4/routes/index.js
@@ -13,4 +13,24 @@ router.get('/last.txt', function(req, res, next) {
   time = now;
 });
 
+var count;
+router.get('/color.html', function(req, res, next) {
+  const color = {'red', 'yellow', 'green', 'blue'};
+  res.send(`<!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <meta charset="utf-8">
+      <title>Express</title>
+    </head>
+
+    <body>
+    <h1 style = "color:$[color[color%4]]">
+    color:$[color[color%4]]
+    </h1>
+    </body>
+
+    </html>`
+    );
+});
+
 module.exports = router;
