8c29a8e7fc83b75ef077fd36c7bdf8768344af3a
seasaur <79822665+seasaur@users.noreply.github.com>
2022-03-30 03:40:41 +0000

    Wed, Mar 30, 2022, 3:40 AM +00:00

---
+++ wdc/prac4_fail/routes/index.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+var time;
+router.get('/last.txt', function(req, res, next) {
+  var now = new Date();
+  res.send(String(time));
+  time = now;
+});
+
+var count;
+router.get('/color.html', function(req, res, next) {
+  const color = {'red', 'yellow', 'green', 'blue'};
+  res.send(`<!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <meta charset="utf-8">
+      <title>Express</title>
+    </head>
+
+    <body>
+    <h1 style = "color:$[color[color%4]]">
+    color:$[color[color%4]]
+    </h1>
+    </body>
+
+    </html>`
+    );
+});
+
+
+module.exports = router;
