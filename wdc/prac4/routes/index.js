d617dd0364032f3579fc784f9d43584b65d2a29c
seasaur <79822665+seasaur@users.noreply.github.com>
2022-03-30 03:46:47 +0000

    Wed, Mar 30, 2022, 3:46 AM +00:00

--- wdc/prac4/routes/index.js
+++ wdc/prac4/routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+var time;
+router.get('/last.txt', function(req, res, next) {
+  var now = new Date();
+  res.send(String(time));
+  time = now;
+});
+
 module.exports = router;
