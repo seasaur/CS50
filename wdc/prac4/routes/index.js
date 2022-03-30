fc01903be385be4c7690cb6ccb7b25b541cae9d7
seasaur <79822665+seasaur@users.noreply.github.com>
2022-03-30 05:02:46 +0000

    Wed, Mar 30, 2022, 5:02 AM +00:00

--- wdc/prac4/routes/index.js
+++ wdc/prac4/routes/index.js
@@ -35,7 +35,7 @@ router.get('/color.html', function(req, res, next) {
 });
 let ff='';
 router.get('/log.html', function(req, res, next) {
-  ff+=`<li>${new Date()}</li>`;
+  ff+=`<li>${new Date()}</li>`; //string concatenation, += adds on top of existing list and doesn't overwrite it
   res.send(`<!DOCTYPE html>
   <html lang="en">
   <head>
