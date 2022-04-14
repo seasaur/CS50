a3a0ade21b5f6843b421a952ef4ad822612dd5ff
seasaur <79822665+seasaur@users.noreply.github.com>
2022-04-14 14:08:27 +0000

    Thu, Apr 14, 2022, 2:08 PM +00:00

--- wdc/prac5/query3-5.sql
+++ wdc/prac5/query3-5.sql
@@ -1,6 +1,6 @@
-SELECT ProductName
-FROM Products
-WHERE ProductID = ALL
-  (SELECT ProductID
-  FROM OrderDetails
-  WHERE Quantity = 10);
+SELECT given_name, family_name
+FROM Students
+WHERE mark = ALL
+  (SELECT student_id
+  FROM Enrolments
+  WHERE mark < 50);
