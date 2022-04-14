55ea27af5ac3b31083c864f22f85f9f1fe13a46a
seasaur <79822665+seasaur@users.noreply.github.com>
2022-04-14 14:16:48 +0000

    Thu, Apr 14, 2022, 2:16 PM +00:00

--- wdc/prac5/query3-6.sql
+++ wdc/prac5/query3-6.sql
@@ -1,3 +1,7 @@
 UPDATE Enrolments
 SET mark = 50
-WHERE student_id='a1111113' subject_code='COMP SCI 1102';
+WHERE student_id='a1111113' AND subject_code='COMP SCI 1102';
+
+UPDATE Enrolments
+SET mark = 65
+WHERE student_id='a1111113' AND subject_code='PHIL 2039';
