467d97366f51ca8d748f0633e570039b7551d569
seasaur <79822665+seasaur@users.noreply.github.com>
2022-04-15 03:04:41 +0000

    Fri, Apr 15, 2022, 3:04 AM +00:00

--- wdc/prac5/query3-4.sql
+++ wdc/prac5/query3-4.sql
@@ -1,9 +1,3 @@
-
-
-SELECT student_id
-FROM Enrolments
-WHERE subject_code='COMP SCI 2207';
-
 SELECT given_name, family_name
 FROM Students
 INNER JOIN Enrolments ON subject_code='COMP SCI 2207' AND Students.student_id = Enrolments.student_id;
