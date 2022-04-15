e2bcd33a00c7e38a3d02073933febbd92e4a01f4
seasaur <79822665+seasaur@users.noreply.github.com>
2022-04-15 03:00:32 +0000

    Fri, Apr 15, 2022, 3:00 AM +00:00

--- wdc/prac5/query3-4.sql
+++ wdc/prac5/query3-4.sql
@@ -2,9 +2,9 @@
 
 SELECT student_id
 FROM Enrolments
-INNER JOIN Students ON Orders.CustomerID = Customers.CustomerID;
 WHERE subject_code='COMP SCI 2207';
 
 SELECT given_name, family_name
 FROM Students
-WHERE student_id='BCompSc';
+INNER JOIN Enrolments ON Students.student_id = Enrolments.student_id;
+
