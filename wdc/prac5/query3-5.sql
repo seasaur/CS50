a8d5d3ddbb35cd5496266cf466e36a6743316df4
seasaur <79822665+seasaur@users.noreply.github.com>
2022-04-15 03:11:48 +0000

    Fri, Apr 15, 2022, 3:11 AM +00:00

--- wdc/prac5/query3-5.sql
+++ wdc/prac5/query3-5.sql
@@ -1,10 +1,3 @@
-SELECT given_name, family_name
-FROM Students
-WHERE student_id = ALL
-  (SELECT student_id
-  FROM Enrolments
-  WHERE mark < 50);
-
 SELECT Students.given_name, Students.family_name, Enrolments.mark
-FROM Enrolments
+FROM Students
 INNER JOIN Enrolments ON subject_code='COMP SCI 2000' AND mark<50 AND Students.student_id = Enrolments.student_id;
