8f9251e7f35cfbcf7e7d576e693093f614f10aa2
seasaur <79822665+seasaur@users.noreply.github.com>
2022-04-21 07:10:56 +0000

    Thu, Apr 21, 2022, 7:10 AM +00:00

--- adds/prac5/main.cpp
+++ adds/prac5/main.cpp
@@ -6,5 +6,9 @@
 int main() {
     FilterGeneric *odd = new FilterOdd();
     std::vector<int> screaming = {1,2,3,4,5,6,7};
-    screaming=FilterOdd
+    std::vector<int> crying;
+    crying=odd->map(screaming);
+    for(int i=0;i<crying.size();i++) {
+        std::cout << crying.at(i) << std::endl;
+    }
 }
