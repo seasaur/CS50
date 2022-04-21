19455a37cef226772feea81cac0191e169796bdd
seasaur <79822665+seasaur@users.noreply.github.com>
2022-04-21 06:43:03 +0000

    Thu, Apr 21, 2022, 6:43 AM +00:00

--- adds/prac5/FilterGeneric.h
+++ adds/prac5/FilterGeneric.h
@@ -1,14 +1,14 @@
 #ifndef FILTERGENERIC_H
 #define FILTERGENERIC_H
+#include <vector>
 
 class FilterGeneric {
     private:
         virtual bool g(int x) = 0;
-        vector<int> store;
+        std::vector<int> store;
 
     public:
         vector<int> filter(vector<int>);
-
-
 };
+
 #endif
