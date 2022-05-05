b5318598c5232653c79d1ea3f7002109b48fa823
seasaur <79822665+seasaur@users.noreply.github.com>
2022-05-05 10:21:23 +0000

    Thu, May 5, 2022, 10:21 AM +00:00

--- adds/prac-exam-2/tail_sumOfCube.cpp
+++ adds/prac-exam-2/tail_sumOfCube.cpp
@@ -1,14 +1,9 @@
 #include <math.h>
 //function that cubes index+1
 
-int sumOfCube(int n, int i) {
+int sumOfCube(int n) {
     if(n<1) {
         return i;
     }
-    return sumOfCube(n-1, i+1);
-}
-
-int sumOfCube(int n) {
-    if
-    return pow(n,3);
+    return sumOfCube(n-1, pow(n,i));
 }
