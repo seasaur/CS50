4e48249af2f205634665765b9ce667de5a49ed15
seasaur <79822665+seasaur@users.noreply.github.com>
2022-05-05 10:30:31 +0000

    Thu, May 5, 2022, 10:30 AM +00:00

--- adds/prac-exam-2/tail_sumOfCube.cpp
+++ adds/prac-exam-2/tail_sumOfCube.cpp
@@ -5,9 +5,9 @@ int sumOfCube(int n, int result) {
     if(n<1) {
         return result;
     }
-    return sumOfCube(n-1, pow(result, 3));
+    return result+sumOfCube(n-1, pow(n, 3));
 }
 
 int sumOfCube(int n) {
-    return pow(n,3);
+    return pow(n,0);
 }
