a9316951aca4a8d0eae2e2393009cff19bbee882
seasaur <79822665+seasaur@users.noreply.github.com>
2022-05-06 03:53:52 +0000

    Fri, May 6, 2022, 3:53 AM +00:00

--- wdc/prac6/prac6.js
+++ wdc/prac6/prac6.js
@@ -54,5 +54,7 @@ var vueinst = new Vue({
             { title:'About',        url:'/about' },
             { title:'Contact Us',   url:'/contact' }
         ]
+        c_text: "type your comment here",
+        c_arr: []
     }
 });
