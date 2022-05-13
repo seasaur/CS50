34f774846d5bb6671a0812552751ce07e9a19c3a
seasaur <79822665+seasaur@users.noreply.github.com>
2022-05-13 07:57:47 +0000

    Fri, May 13, 2022, 7:57 AM +00:00

--- wdc/group/eventDetails.js
+++ wdc/group/eventDetails.js
@@ -35,7 +35,11 @@ if (table != null) {
     for (var i = 0; i < table.rows.length; i++) {
         for (var j = 0; j < table.rows[i].cells.length; j++)
         table.rows[i].cells[j].onclick = function () {
-            tableText(this);
+            toggleTick(this);
         };
     }
+}
+
+function toggleTick(tableCell) {
+    if
 }
