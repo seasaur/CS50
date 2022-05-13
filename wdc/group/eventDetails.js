e60af905a8160f1f0102e74c030bc589fc4da01d
seasaur <79822665+seasaur@users.noreply.github.com>
2022-05-13 07:55:18 +0000

    Fri, May 13, 2022, 7:55 AM +00:00

--- wdc/group/eventDetails.js
+++ wdc/group/eventDetails.js
@@ -35,7 +35,7 @@ if (table != null) {
     for (var i = 0; i < table.rows.length; i++) {
         for (var j = 0; j < table.rows[i].cells.length; j++)
         table.rows[i].cells[j].onclick = function () {
-            tableText(this);
+            if 
         };
     }
 }
