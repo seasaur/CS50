4d5af06f427c5a6c82c97ce52d4cfc0536eef14d
seasaur <79822665+seasaur@users.noreply.github.com>
2022-05-17 08:31:39 +0000

    Tue, May 17, 2022, 8:31 AM +00:00

--- wdc/prac7/routes/index.js
+++ wdc/prac7/routes/index.js
@@ -6,9 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-// router.get('/prac8', function(req, res, next) {
-//   res.render('prac8_1-3', { title: 'helo' });
-// });
+router.get('/prac8', function(req, res, next) {
+  res.render('prac8_1-4', { title: 'helo' });
+});
 
 router.get('/brew', function(req, res) {
   var q = req.query.drink;
