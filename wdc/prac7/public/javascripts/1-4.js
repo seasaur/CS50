ed3908ba941e6ccdc77dc9a2a5943ca396c15ec1
seasaur <79822665+seasaur@users.noreply.github.com>
2022-05-17 11:55:07 +0000

    Tue, May 17, 2022, 11:55 AM +00:00

--- wdc/prac7/public/javascripts/1-4.js
+++ wdc/prac7/public/javascripts/1-4.js
@@ -3,16 +3,8 @@ function makePost() {
     var title = document.getElementById("title").value;
    var content = document.getElementById("content").value;
 
-   var xhttp = new XMLHttpRequest();
-   xhttp.onreadystatechange = function() {
-       if (this.readyState == 4 && this.status == 200) {
-           console.log("done");
-           //getPosts();
-       }
-   };
-   xhttp.open("POST", "/addpost", true);
-   xhttp.setRequestHeader("Content-type", "application/json");
-   xhttp.send(JSON.stringify({ title: title, content: content }));
+   addPost(title,content);
+
 }
 
 var postList = [];
