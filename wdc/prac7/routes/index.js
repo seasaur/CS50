93f291b70c0086a091ec010666327d029beb267b
seasaur <79822665+seasaur@users.noreply.github.com>
2022-05-17 02:59:09 +0000

    Tue, May 17, 2022, 2:59 AM +00:00

--- wdc/prac7/routes/index.js
+++ wdc/prac7/routes/index.js
@@ -8,12 +8,15 @@ router.get('/', function(req, res, next) {
 
 router.get('/brew', function(req, res) {
   var drink = req.query.param1;
-  if (param1==tea) {
-  res.send('A delicious cup of tea!'+q);
+  if (param1=='tea') {
+  res.send('A delicious cup of tea!'+drink);
   }
-  if (param1==coffee) {
-    res.sendStatus(403);;
-    }
+  if (param1=='coffee') {
+    res.sendStatus(418);
+  }
+  else {
+    res.sendStatus(400);
+  }
 });
 
 module.exports = router;
