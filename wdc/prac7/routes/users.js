6191983c1d915f47caa78a5678815f5f179ccd2c
seasaur <79822665+seasaur@users.noreply.github.com>
2022-05-18 02:48:16 +0000

    Wed, May 18, 2022, 2:48 AM +00:00

--- wdc/prac7/routes/users.js
+++ wdc/prac7/routes/users.js
@@ -8,15 +8,18 @@ router.get('/', function(req, res, next) {
 
 var posts = [];
 router.post('/addpost', function(req, res) {
-  var jsonPosts = JSON.parse(posts);
-  jsonPosts.unshift(req.body.title);
-  var title= req.body.title
-  posts=JSON.parse(jsonPosts);
-  posts.push()
+  // var jsonPosts = JSON.parse(posts);
+  // jsonPosts.unshift(req.body.title);
+  var post={
+    title: req.body.title,
+    content: req.body.content
+  }
+  posts.push(post)
   res.send(posts);
 });
 
 router.get('/getposts', function(req,res) {
+  
   res.send(posts);
 });
 
