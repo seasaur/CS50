87688a1651820415f817aaee19b57f47c3d3e6dc
seasaur <79822665+seasaur@users.noreply.github.com>
2022-05-18 10:16:11 +0000

    Wed, May 18, 2022, 10:16 AM +00:00

--- wdc/prac7/app.js
+++ wdc/prac7/app.js
@@ -40,7 +40,7 @@ app.use('/users', usersRouter);
 // //  }
 // //  })
 
-// app.use("/users/*",function(req,res) {
+// app.use("/*",function(req,res) {
 //     if(req.header.content-type != "Application/json") {
 //         req.send(412);
 //      }
