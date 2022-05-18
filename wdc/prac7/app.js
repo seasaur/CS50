a35cc0fdaf047d189f7900dc418ce32456cb7fdb
seasaur <79822665+seasaur@users.noreply.github.com>
2022-05-18 08:36:59 +0000

    Wed, May 18, 2022, 8:36 AM +00:00

--- wdc/prac7/app.js
+++ wdc/prac7/app.js
@@ -48,10 +48,10 @@ app.use('/users', usersRouter);
 
 app.get('/cookie', function(req, res) {
     if (req.cookies.task3_1===null) {
-      res.cookie('task3_1', ++req.cookies.task3_1);
+      res.cookie('task3_1', 1);
     }
     else {
-      req.cookies.task3_1+=1;
+      res.cookie('task3_1', ++req.cookies.task3_1);
     }
     res.send();
   });
