00b479dc3df6167d6aaa84177f905c4f2672a7d6
seasaur <79822665+seasaur@users.noreply.github.com>
2022-05-23 03:47:29 +0000

    Mon, May 23, 2022, 3:47 AM +00:00

--- wdc/prac8/query1-4.sql
+++ wdc/prac8/query1-4.sql
@@ -1,6 +1,6 @@
 SELECT inventory.film_id store.store_id
 FROM film
-INNER JOIN inventory ON film_id='918' AND mark<50 AND Students.student_id = Enrolments.student_id;
+INNER JOIN inventory ON film_id='918' AND mark<50 AND inventory.store_id = store.store_id;
 
 - film id 918
 - address of every store with the film id
