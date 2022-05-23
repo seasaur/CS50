d461b70ea793e4e21121e3f57f053bb72697c14b
seasaur <79822665+seasaur@users.noreply.github.com>
2022-05-23 03:14:25 +0000

    Mon, May 23, 2022, 3:14 AM +00:00

--- wdc/prac8/query1-4.sql
+++ wdc/prac8/query1-4.sql
@@ -1,4 +1,4 @@
-SELECT film.film_id, film.title, inventory.
+SELECT film.film_id, film.title, inventory.film_id
 FROM film
 INNER JOIN inventory ON film_id='918' AND mark<50 AND Students.student_id = Enrolments.student_id;
 
