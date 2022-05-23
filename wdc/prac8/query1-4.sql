3b8efaaa2c6913e3ff1c016f53bb3477ac6c9312
seasaur <79822665+seasaur@users.noreply.github.com>
2022-05-23 03:10:07 +0000

    Mon, May 23, 2022, 3:10 AM +00:00

--- wdc/prac8/query1-4.sql
+++ wdc/prac8/query1-4.sql
@@ -1,3 +1,4 @@
 SELECT Students.given_name, Students.family_name, Enrolments.mark
 FROM Students
 INNER JOIN Enrolments ON subject_code='COMP SCI 2000' AND mark<50 AND Students.student_id = Enrolments.student_id;
+
