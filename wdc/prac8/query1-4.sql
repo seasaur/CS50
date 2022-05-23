d87bf305f6fb2b438b1b0c48fa62bf312b3d528b
seasaur <79822665+seasaur@users.noreply.github.com>
2022-05-23 03:49:32 +0000

    Mon, May 23, 2022, 3:49 AM +00:00

--- wdc/prac8/query1-4.sql
+++ wdc/prac8/query1-4.sql
@@ -4,4 +4,5 @@ INNER JOIN inventory ON film_id='918' AND mark<50 AND inventory.store_id = store
 
 - film id 918
 - address of every store with the film id
+- get address from address table from address id
 - only one address per store
