25ca432a4217fc30d30046e4407431585011744d
seasaur <79822665+seasaur@users.noreply.github.com>
2022-05-23 05:12:42 +0000

    Mon, May 23, 2022, 5:12 AM +00:00

--- wdc/prac8/query1-3.sql
+++ wdc/prac8/query1-3.sql
@@ -2,6 +2,6 @@ SELECT customer.first_name, customer.last_name, rental.rental_date
 FROM customer
 INNER JOIN rental
 ON customer.customer_id=rental.customer_id
-WHERE rental.return_date=null
 ORDER BY rental_date ASC
+WHERE rental.return_date=0
 LIMIT 1;
