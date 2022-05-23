dbcde2cf679d11c131a6685ac7464fec7898ad43
seasaur <79822665+seasaur@users.noreply.github.com>
2022-05-23 04:11:34 +0000

    Mon, May 23, 2022, 4:11 AM +00:00

--- wdc/prac8/query1-4.sql
+++ wdc/prac8/query1-4.sql
@@ -5,5 +5,5 @@ ON film_id='918' AND mark<50 AND inventory.store_id = store.store_id;
 
 - film id 918
 - stores table- get address id that has film id (only one address id, no duplicates)
-- get address from address table from address id
+- address table- get address from address table from address id
 - only one address per store
