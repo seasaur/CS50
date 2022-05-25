6bf8a705605809e42e492f0d378e271cbc834fbd
seasaur <79822665+seasaur@users.noreply.github.com>
2022-05-25 03:27:21 +0000

    Wed, May 25, 2022, 3:27 AM +00:00

--- wdc/prac8/query1-3.sql
+++ wdc/prac8/query1-3.sql
@@ -2,5 +2,6 @@ SELECT customer.first_name, customer.last_name, rental.rental_date
 FROM customer
 INNER JOIN rental
 ON customer.customer_id=rental.customer_id
+WHERE rental.return_date is NULL
 ORDER BY rental.rental_date ASC
 LIMIT 1;
