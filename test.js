891489119d30f714596494f1fd66581ab63d43cd
seasaur <79822665+seasaur@users.noreply.github.com>
2022-05-28 07:21:25 +0000

    Sat, May 28, 2022, 7:21 AM +00:00

--- test.js
+++ test.js
@@ -5,3 +5,9 @@ let d = null;
 let e = { hello: 'hi' };
 let f = '';
 
+if( e.hello === true ){ console.log( "true" ) } else { console.log( "false" ) };
+if( f === d === true ){ console.log( "true" ) } else { console.log( "false" ) };
+if( e.hi === true ){ console.log( "true" ) } else { console.log( "false" ) };
+if( !b === true ){ console.log( "true" ) } else { console.log( "false" ) };
+if( c == f === true ){ console.log( "true" ) } else { console.log( "false" ) };
+if( a === true ){ console.log( "true" ) } else { console.log( "false" ) };
