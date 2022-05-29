bd3383059ba26fb8f340aa94c3d5ffdd257be74e
seasaur <79822665+seasaur@users.noreply.github.com>
2022-05-29 12:07:36 +0000

    Sun, May 29, 2022, 12:07 PM +00:00

--- wdc/group/account.js
+++ wdc/group/account.js
@@ -11,3 +11,47 @@ var vueinst = new Vue({
         ],
     }
 });
+
+var actorArray = [];
+function get_list(){
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        actorArray = JSON.parse(this.responseText);
+        for (var i = 0; i < actorArray.length; i++) {
+            var list = document.getElementById("actors");
+            var newRow = document.createElement("tr");
+            var newActor_first_name = document.createElement("td");
+            newActor_first_name.innerHTML = actorArray[i].first_name;
+            newRow.appendChild(newActor_first_name);
+            var newActor_last_name = document.createElement("td");
+            newActor_last_name.innerHTML = actorArray[i].last_name;
+            newRow.appendChild(newActor_last_name);
+            list.appendChild(newRow);
+        }
+      }
+    };
+
+    xhttp.open("GET", "/actors", true);
+    xhttp.send();
+
+}
+
+var newActorArray = [];
+
+function add_actor(){
+  var first_name = document.getElementById("actor-first-name").value;;
+  var last_name = document.getElementById("actor-last-name").value;
+
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      }
+    };
+
+  xhttp.open("POST", "/add", true);
+  xhttp.setRequestHeader("Content-type", "application/json");
+  xhttp.send(JSON.stringify({first_name: first_name, last_name: last_name}));
+
+}
