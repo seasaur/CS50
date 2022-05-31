c1ad99f794dab7e17e00bb106601fceca2ddff80
seasaur <79822665+seasaur@users.noreply.github.com>
2022-05-31 13:48:28 +0000

    Tue, May 31, 2022, 1:48 PM +00:00

--- wdc/group/routes/index.js
+++ wdc/group/routes/index.js
@@ -14,7 +14,7 @@ router.get('/account', function(req, res, next) {
       return;
     }
 
-    let query = "SELECT Users.username, Users.password, Notification.email";
+    let query = "SELECT Users.username, Users.first_name, Users.password, Notification.email";
     connection.query(query, function(error, rows, fields) {
       connection.release();
       if(error){
@@ -35,7 +35,7 @@ router.post('/add', function(req, res, next) {
       return;
     }
 
-    let query = "INSERT INTO actor (first_name, last_name) VALUES(?,?);";
+    let query = "INSERT INTO Users (username, first_name, password) VALUES(?,?);";
     connection.query(query, [req.body.first_name, req.body.last_name], function(error, rows, fields) {
       connection.release();
       if(error){
