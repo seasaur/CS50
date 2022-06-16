c1dd6f300a37da5335b8996b479bc4386ba1e8a5
seasaur <79822665+seasaur@users.noreply.github.com>
2022-06-16 09:16:00 +0000

    Thu, Jun 16, 2022, 9:16 AM +00:00

--- wdc/exam/public/javascripts/search.js
+++ wdc/exam/public/javascripts/search.js
@@ -22,50 +22,50 @@ function getDates(){
 
 
 
-var seatList = [];
-function getSeats(){
-    document.getElementById("seats").innerHTML = ""; //clear out previous search results first
-    var select = document.getElementById("searchName").value;
-    var selected = select.options[select.selectedIndex].text
-    console.log(selected);
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        seatList = JSON.parse(this.responseText);
-        console.log(seatList);
-        if(seatList.length==0) {
-            alert("There are no screen times matching your selected filters.");
-            break;
-        }
-        for (var i = 0; i < seatList.length; i++) {
-            var list = document.getElementById("seats");
-            var newDiv = document.createElement("div");
-            newDiv.setAttribute('class', 'eventBox');
-            var filmName = document.createElement("h2");
-            filmName.setAttribute('class', 'boxHeading');
-            filmName.innerHTML = seatList[i].filmName;
-            newDiv.appendChild(filmName);
+// var seatList = [];
+// function getSeats(){
+//     document.getElementById("seats").innerHTML = ""; //clear out previous search results first
+//     var select = document.getElementById("searchName").value;
+//     var selected = select.options[select.selectedIndex].text
+//     console.log(selected);
+//     var xhttp = new XMLHttpRequest();
+//     xhttp.onreadystatechange = function() {
+//       if (this.readyState == 4 && this.status == 200) {
+//         seatList = JSON.parse(this.responseText);
+//         console.log(seatList);
+//         if(seatList.length==0) {
+//             alert("There are no screen times matching your selected filters.");
+//             return;
+//         }
+//         for (var i = 0; i < seatList.length; i++) {
+//             var list = document.getElementById("seats");
+//             var newDiv = document.createElement("div");
+//             newDiv.setAttribute('class', 'eventBox');
+//             var filmName = document.createElement("h2");
+//             filmName.setAttribute('class', 'boxHeading');
+//             filmName.innerHTML = seatList[i].filmName;
+//             newDiv.appendChild(filmName);
 
-            var button = document.createElement("button");
-            button.innerHTML = "VIEW USER";
-            button.id = seatList[i].filmName;
-            button.setAttribute('class', 'submitButton');
+//             var button = document.createElement("button");
+//             button.innerHTML = "VIEW USER";
+//             button.id = seatList[i].filmName;
+//             button.setAttribute('class', 'submitButton');
 
-            console.log(button.id, "filmName");
+//             console.log(button.id, "filmName");
 
 
-            button.onclick = function(user) {
-              viewAUser(this.id)
-           }
+//             button.onclick = function(user) {
+//               viewAUser(this.id)
+//            }
 
-            newDiv.appendChild(button);
-            list.appendChild(newDiv);
+//             newDiv.appendChild(button);
+//             list.appendChild(newDiv);
 
-        }
-      }
-    };
+//         }
+//       }
+//     };
 
-    xhttp.open("GET", "/searchSeats", true);
-    xhttp.send();
+//     xhttp.open("GET", "/searchSeats", true);
+//     xhttp.send();
 
-}
+// }
