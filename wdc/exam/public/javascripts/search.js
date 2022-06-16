db3d945f2f0ea95df09dcc1494d256b28cb7dbb3
seasaur <79822665+seasaur@users.noreply.github.com>
2022-06-16 11:33:45 +0000

    Thu, Jun 16, 2022, 11:33 AM +00:00

--- wdc/exam/public/javascripts/search.js
+++ wdc/exam/public/javascripts/search.js
@@ -1,3 +1,5 @@
+
+//onload: generating the dropdown list for dates
 var dateList = [];
 function getDates(){
     var xhttp = new XMLHttpRequest();
@@ -26,7 +28,7 @@ function getDates(){
     getFilms();
 }
 
-
+//onload: generating the dropdown list for films (called by getDates())
 var filmList = [];
 function getFilms(){
     var xhttp = new XMLHttpRequest();
@@ -51,51 +53,51 @@ function getFilms(){
 
 
 
+//search function
+var seatList = [];
+function getSeats(){
+    document.getElementById("seats").innerHTML = ""; //clear out previous search results first
+    var select = document.getElementById("searchName").value;
+    var selected = select.options[select.selectedIndex].text
+    console.log(selected);
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        seatList = JSON.parse(this.responseText);
+        console.log(seatList);
+        if(seatList.length==0) {
+            alert("There are no screen times matching your selected filters.");
+            return;
+        }
+        for (var i = 0; i < seatList.length; i++) {
+            var list = document.getElementById("seats");
+            var newDiv = document.createElement("div");
+            newDiv.setAttribute('class', 'eventBox');
+            var filmName = document.createElement("h2");
+            filmName.setAttribute('class', 'boxHeading');
+            filmName.innerHTML = seatList[i].filmName;
+            newDiv.appendChild(filmName);
 
-// var seatList = [];
-// function getSeats(){
-//     document.getElementById("seats").innerHTML = ""; //clear out previous search results first
-//     var select = document.getElementById("searchName").value;
-//     var selected = select.options[select.selectedIndex].text
-//     console.log(selected);
-//     var xhttp = new XMLHttpRequest();
-//     xhttp.onreadystatechange = function() {
-//       if (this.readyState == 4 && this.status == 200) {
-//         seatList = JSON.parse(this.responseText);
-//         console.log(seatList);
-//         if(seatList.length==0) {
-//             alert("There are no screen times matching your selected filters.");
-//             return;
-//         }
-//         for (var i = 0; i < seatList.length; i++) {
-//             var list = document.getElementById("seats");
-//             var newDiv = document.createElement("div");
-//             newDiv.setAttribute('class', 'eventBox');
-//             var filmName = document.createElement("h2");
-//             filmName.setAttribute('class', 'boxHeading');
-//             filmName.innerHTML = seatList[i].filmName;
-//             newDiv.appendChild(filmName);
+            var button = document.createElement("button");
+            button.innerHTML = "VIEW USER";
+            button.id = seatList[i].filmName;
+            button.setAttribute('class', 'submitButton');
 
-//             var button = document.createElement("button");
-//             button.innerHTML = "VIEW USER";
-//             button.id = seatList[i].filmName;
-//             button.setAttribute('class', 'submitButton');
-
-//             console.log(button.id, "filmName");
+            console.log(button.id, "filmName");
 
 
-//             button.onclick = function(user) {
-//               viewAUser(this.id)
-//            }
+            button.onclick = function(user) {
+              viewAUser(this.id)
+           }
 
-//             newDiv.appendChild(button);
-//             list.appendChild(newDiv);
+            newDiv.appendChild(button);
+            list.appendChild(newDiv);
 
-//         }
-//       }
-//     };
+        }
+      }
+    };
 
-//     xhttp.open("GET", "/searchSeats", true);
-//     xhttp.send(JSON.stringify(selected)); //or just selected??
+    xhttp.open("GET", "/searchSeats", true);
+    xhttp.send(JSON.stringify(selected)); //or just selected??
 
-// }
+}
