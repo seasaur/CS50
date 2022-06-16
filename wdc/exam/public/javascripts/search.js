5e1bf2290586b79fa3cbe8a98c8676f86cfe10b5
seasaur <79822665+seasaur@users.noreply.github.com>
2022-06-16 08:25:23 +0000

    Thu, Jun 16, 2022, 8:25 AM +00:00

--- wdc/exam/public/javascripts/search.js
+++ wdc/exam/public/javascripts/search.js
@@ -2,7 +2,8 @@
 var seatList = [];
 function getSeats(){
     document.getElementById("seats").innerHTML = ""; //clear out previous search results first
-    var search = document.getElementById("userNameSearch").value;
+    var search = document.getElementById("searchName").selected;
+    console.log(search);
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
