3de21d1c7c026c6d7c65ee827ecde21c4aa09371
seasaur <79822665+seasaur@users.noreply.github.com>
2022-06-16 03:27:20 +0000

    Thu, Jun 16, 2022, 3:27 AM +00:00

--- wdc/prac8/sakila.sql
+++ wdc/prac8/sakila.sql
@@ -332,17 +332,7 @@ CREATE TABLE staff (
 -- Table structure for table `store`
 --
 
-CREATE TABLE store (
-  store_id TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  manager_staff_id TINYINT UNSIGNED NOT NULL,
-  address_id SMALLINT UNSIGNED NOT NULL,
-  last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY  (store_id),
-  UNIQUE KEY idx_unique_manager (manager_staff_id),
-  KEY idx_fk_address_id (address_id),
-  CONSTRAINT fk_store_staff FOREIGN KEY (manager_staff_id) REFERENCES staff (staff_id) ON DELETE RESTRICT ON UPDATE CASCADE,
-  CONSTRAINT fk_store_address FOREIGN KEY (address_id) REFERENCES address (address_id) ON DELETE RESTRICT ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 
 --
 -- View structure for view `customer_list`
