60737a40255fb8add4f1c1ebeb87d8fab5eeb311
seasaur <79822665+seasaur@users.noreply.github.com>
2022-06-16 13:54:27 +0000

    Thu, Jun 16, 2022, 1:54 PM +00:00

--- wdc/exam/routes/index.js
+++ wdc/exam/routes/index.js
@@ -78,14 +78,8 @@ router.get('/searchSeats', function(req, res, next) {
         res.sendStatus(500);
         return;
       }
-      // if(rows.length>0){
         console.log(rows, "ah");
         res.json(rows);
-      // }
-      // else{
-      //   res.sendStatus(401);
-      // }
-
     });
   });
 });
@@ -106,14 +100,30 @@ router.get('/any', function(req, res, next) {
         res.sendStatus(500);
         return;
       }
-      // if(rows.length>0){
         console.log(rows, "ah");
         res.json(rows);
-      // }
-      // else{
-      //   res.sendStatus(401);
-      // }
+    });
+  });
+});
 
+router.get('/datesAny', function(req, res, next) {
+  req.pool.getConnection(function(error,connection){
+    if(error){
+      console.log(error);
+      res.sendStatus(500);
+      return;
+    }
+
+    let query = "SELECT Films.name, Films.dates, Films.time, Seats.seat FROM Seats INNER JOIN Films ON Seats.filmID=Films.filmID WHERE dates = ?;";
+    connection.query(query, [dates, name], function(error, rows, fields) {
+      connection.release();
+      if(error){
+        console.log(error);
+        res.sendStatus(500);
+        return;
+      }
+        console.log(rows, "ah");
+        res.json(rows);
     });
   });
 });
