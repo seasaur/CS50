cec29e63b5d639ee6be196ac50c2f68dceecb02e
seasaur <79822665+seasaur@users.noreply.github.com>
2022-06-16 06:21:19 +0000

    Thu, Jun 16, 2022, 6:21 AM +00:00

--- wdc/exam/sqlCommands.sql
+++ wdc/exam/sqlCommands.sql
@@ -49,4 +49,4 @@ INSERT INTO Seats VALUES (8, 'D02', false, 4);
 INSERT INTO Bookings VALUES (1, 1);
 INSERT INTO Bookings VALUES (3, 1);
 
-SELECT user_ID, first_name, is_admin FROM Users WHERE google_ID = ?;
+SELECT Films.name, Seats.seat FROM Users WHERE Date = '2022-06-20';
