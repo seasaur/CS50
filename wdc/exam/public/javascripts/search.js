6d17554b9406f726f058f5d3adc06aff553efa03
seasaur <79822665+seasaur@users.noreply.github.com>
2022-06-16 08:06:14 +0000

    Thu, Jun 16, 2022, 8:06 AM +00:00

--- wdc/exam/public/javascripts/search.js
+++ wdc/exam/public/javascripts/search.js
@@ -1,26 +1,28 @@
 
-var filmList = [];
+var seatList = [];
 function getUsers(){
     document.getElementById("users").innerHTML = ""; //clear out previous search results first
     var searchBar = document.getElementById("userNameSearch").value;
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-        filmList = JSON.parse(this.responseText);
-        console.log(filmList);
-        if(filmList)
-        for (var i = 0; i < filmList.length; i++) {
+        seatList = JSON.parse(this.responseText);
+        console.log(seatList);
+        if(seatList.length==0) {
+            
+        }
+        for (var i = 0; i < seatList.length; i++) {
             var list = document.getElementById("users");
             var newDiv = document.createElement("div");
             newDiv.setAttribute('class', 'eventBox');
             var username = document.createElement("h2");
             username.setAttribute('class', 'boxHeading');
-            username.innerHTML = filmList[i].username;
+            username.innerHTML = seatList[i].username;
             newDiv.appendChild(username);
 
             var button = document.createElement("button");
             button.innerHTML = "VIEW USER";
-            button.id = filmList[i].username;
+            button.id = seatList[i].username;
             button.setAttribute('class', 'submitButton');
 
             console.log(button.id, "username");
