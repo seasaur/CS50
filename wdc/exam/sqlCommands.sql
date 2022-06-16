76a952a2b9ada9e5694b587e651f423343a816a8
seasaur <79822665+seasaur@users.noreply.github.com>
2022-06-16 08:53:03 +0000

    Thu, Jun 16, 2022, 8:53 AM +00:00

--- wdc/exam/sqlCommands.sql
+++ wdc/exam/sqlCommands.sql
@@ -53,4 +53,4 @@ mysqldump --host=127.0.0.1 --databases Cinema > cinema.sql
 
 --queries--
 --to get dates and append to list
-SELECT Events.event_ID, Events.event_name FROM Events INNER JOIN Hosts ON Events.event_ID = Hosts.event_ID WHERE Hosts.user_ID = ?;
+SELECT dates FROM Films;
