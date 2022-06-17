eb99586c74028e897226360881f8006bf349abe2
seasaur <79822665+seasaur@users.noreply.github.com>
2022-06-17 02:30:32 +0000

    Fri, Jun 17, 2022, 2:30 AM +00:00

--- wdc/exam/public/javascripts/search.js
+++ wdc/exam/public/javascripts/search.js
@@ -239,9 +239,9 @@ function book() {
       alert("You did not select any seats.");
       return;
     }
-    for(var i=0;i<bookedList;i++) {
-        send(bookedList[i]);
-    }
+    // for(var i=0;i<bookedList;i++) {
+    //     send(bookedList[i]);
+    // }
 }
 
 function send(booked) {
