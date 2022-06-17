7011633e6b5623b30e5e94ceee51afc36dd2713e
seasaur <79822665+seasaur@users.noreply.github.com>
2022-06-17 07:42:15 +0000

    Fri, Jun 17, 2022, 7:42 AM +00:00

--- wdc/part_2/routes/index.js
+++ wdc/part_2/routes/index.js
@@ -35,55 +35,6 @@ router.get('/posts', function(req, res, next) {
   });
 
 });
-
-//added
-router.use(function(req,res,next) {
-  if('user' in req.session) {
-    next();
-  }
-  else {
-    res.sendStatus(401);
-  }
-});
-
-
-router.post('/posts/new', function(req, res, next) {
-
-    // if(!('user' in req.session)){
-    //   res.sendStatus(401);
-    //   return;
-    // }
-
-    if( "title" in req.body && req.body.title !== null &&
-        "content" in req.body && req.body.content !== null ) {
-        req.body.author = req.session.user;
-
-        req.pool.getConnection( function(err,connection) {
-            if (err) {
-              res.sendStatus(500);
-              console.error(err);
-              return;
-            }
-            var query = `INSERT INTO posts (author,title,content,timestamp) VALUES (?,?,?,NOW());`;
-            connection.query(query, [req.body.author.u_id,req.body.title,sanitizeHtml(req.body.content)], function(err, rows, fields) {
-                if (err) {
-                    res.sendStatus(500);
-                    console.error(err);
-                    connection.release(); // release connection if error
-                    return;
-                }
-
-                res.end(); //send response
-
-            });
-        });
-
-    } else {
-        res.sendStatus(400);
-    }
-
-});
-
 router.get('/posts/:post_id/comments', function(req, res, next) {
 
   if('user' in req.session){
@@ -117,6 +68,59 @@ router.get('/posts/:post_id/comments', function(req, res, next) {
 
 });
 
+router.use('/posts/*', function(req, res, next) {
+  if('user' in req.session){
+      next();
+  } else {
+      res.sendStatus(401);
+  }
+});
+
+router.use('/comments/new', function(req, res, next) {
+  if('user' in req.session){
+      next();
+  } else {
+      res.sendStatus(401);
+  }
+});
+
+
+router.post('/posts/new', function(req, res, next) {
+
+  // if(!('user' in req.session)){
+  //   res.sendStatus(401);
+  //   return;
+  // }
+    if( "title" in req.body && req.body.title !== null &&
+        "content" in req.body && req.body.content !== null ) {
+        req.body.author = req.session.user;
+
+        req.pool.getConnection( function(err,connection) {
+            if (err) {
+              res.sendStatus(500);
+              console.error(err);
+              return;
+            }
+            var query = INSERT INTO posts (author,title,content,timestamp) VALUES (?,?,?,NOW());;
+            connection.query(query, [req.body.author.u_id,req.body.title,sanitizeHtml(req.body.content)], function(err, rows, fields) {
+                if (err) {
+                    res.sendStatus(500);
+                    console.error(err);
+                    connection.release(); // release connection if error
+                    return;
+                }
+
+                res.end(); //send response
+
+            });
+        });
+
+    } else {
+        res.sendStatus(400);
+    }
+
+});
+
 
 
 router.post('/posts/:post_id/delete', function(req, res, next) {
@@ -125,7 +129,6 @@ router.post('/posts/:post_id/delete', function(req, res, next) {
   //   res.sendStatus(401);
   //   return;
   // }
-
   switch(req.session.user.role){
     case 'user':
       res.sendStatus(401);
@@ -138,12 +141,17 @@ router.post('/posts/:post_id/delete', function(req, res, next) {
   }
 
   req.pool.getConnection( function(err,connection) {
+
+  // if(!('user' in req.session)){
+  //   res.sendStatus(401);
+  //   return;
+  // }
       if (err) {
         res.sendStatus(500);
         console.error(err);
         return;
       }
-      var query = `DELETE FROM posts WHERE p_id=?;`;
+      var query = DELETE FROM posts WHERE p_id=?;;
       connection.query(query, [req.params.post_id], function(err, rows, fields) {
         connection.release(); // release connection
         if (err) {
@@ -157,25 +165,12 @@ router.post('/posts/:post_id/delete', function(req, res, next) {
 
 });
 
-//added
-router.use('/posts/*', function(req,res,next) {
-  if('user' in req.session) {
-    next();
-  }
-  else {
-    res.sendStatus(401);
-  }
-});
-
-
-
 router.post('/comments/new', function(req, res, next) {
 
   // if(!('user' in req.session)){
   //   res.sendStatus(401);
   //   return;
   // }
-
   if( "title" in req.body && req.body.title !== null &&
       "content" in req.body && req.body.content !== null &&
       "post_id" in req.body && req.body.post_id !== null ) {
@@ -187,7 +182,7 @@ router.post('/comments/new', function(req, res, next) {
             // console.error(err);
             return;
           }
-          var query = `INSERT INTO questions (author,title,content,timestamp,post) VALUES (?,?,?,NOW());`;
+          var query = INSERT INTO questions (author,title,content,timestamp,post) VALUES (?,?,?,NOW());;
           connection.query(query, [req.body.author.u_id,req.body.title,req.body.content,req.body.post_id], function(err, rows, fields) {
               if (err) {
                   res.sendStatus(500);
@@ -206,6 +201,8 @@ router.post('/comments/new', function(req, res, next) {
   }
 
 });
+
+
 
 
 module.exports = router;
