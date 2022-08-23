37719b4cbf3a6cae1aa320d61fad248356a76661
seasaur <79822665+seasaur@users.noreply.github.com>
2022-08-23 12:10:57 +0000

    ddd, Aug 8/23/2022, YYYY, 12:10 A +0

--- adsa/assignment1/main.cpp
+++ adsa/assignment1/main.cpp
@@ -1,12 +1,15 @@
 #include <string>
 #include <iostream>
-#include <cstdlib>
+#include <math.h>
 using namespace std;
+
 
 // Function to find the sum of
 // two integers of base B
-string sumBaseB(string a, string b, int base)
-{
+string sumBaseB(int x, int y, int base) {
+    string a = to_string(x);
+    string b = to_string(y);
+
     int aSize, bSize;
 
     aSize = a.size();
@@ -40,8 +43,7 @@ string sumBaseB(string a, string b, int base)
     for (int i=start; i>-1; i--) {
 
         // current value
-        current = carry + (a[i] - '0') +
-                       (b[i] - '0');
+        current = carry + (a[i] - '0') + (b[i] - '0');
 
         // carry
         carry = current / base;
@@ -57,21 +59,60 @@ string sumBaseB(string a, string b, int base)
     return sum;
 }
 
+int getSize(long num)
+{
+    int count = 0;
+    while (num > 0)
+    {
+        count++;
+        num /= 10;
+    }
+    return count;
+}
 
+long karatsuba(long x, long y)
+{
+    // Base Case
+    if (x < 10 && y < 10)
+        return x * y;
 
+    // determine the size of x and y
+    int size = fmax(getSize(x), getSize(y));
 
+    // Split x and y
+    int n = ceil(size / 2);
+    long p = pow(10, n);
+    long a = floor(x / p);
+    long b = x % p;
+    long c = floor(y / p);
+    long d = y % p;
 
-int main(){
-    string a = "";
-    string b = "";
+    // Recur until base case
+    long ac = karatsuba(a, c);
+    long bd = karatsuba(b, d);
+    long e = karatsuba(a + b, c + d) - ac - bd;
+
+    // return the equation
+    return (pow(10, 2 * n) * ac + pow(10, n) * e + bd);
+}
+
+int main() {
+    // string a = "";
+    // string b = "";
     string sum = "";
-    string product = "";
+    // string product = "";
     int base;
+
+
+    int a;
+    int b;
+    int product;
+
     cin >> a >> b >> base;
 
     // Function Call
     sum = sumBaseB(a, b, base);
-    // product = multiply(a,b);
+    product = karatsuba(a,b);
     cout << sum << " " << product << endl;
     return 0;
 }
