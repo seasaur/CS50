6493c8edf4d80e77b73478414446ac186a3e7b4d
seasaur <79822665+seasaur@users.noreply.github.com>
2022-08-24 07:18:22 +0000

    ddd, Aug 8/24/2022, YYYY, 7:18 A +0

--- adsa/assignment1/main.cpp
+++ adsa/assignment1/main.cpp
@@ -64,7 +64,7 @@ string subtract(string left, string right, int base) {
             int j = i - 1;
             while (j > -1) {
                 left[j] = ((left[j]-'0') - 1) % base + '0';
-                if (left[j] != base-1 + '0') {
+                if (left[j] != (base + '0')-1) {
                     break;
                 }
                 else {
