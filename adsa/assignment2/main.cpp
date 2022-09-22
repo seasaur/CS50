4d94b6f6b228e1958ed063116092eebec8aa249d
seasaur <79822665+seasaur@users.noreply.github.com>
2022-09-22 08:19:17 +0000

    ddd, Sep 9/22/2022, YYYY, 8:19 A +0

--- adsa/assignment2/main.cpp
+++ adsa/assignment2/main.cpp
@@ -181,30 +181,33 @@ bool ifNodeExists(Node* node, int key) {
 }
 
 Node * deleteNode(struct Node *root,int data) {
-    //check if node exists
     if (root == NULL) {
         return root;
     }
 
-    //find data
-    //left or right
     if (data < root->data) {
         root->left = deleteNode(root->left,data);
-    } else if (data >  root->data) {
+    }
+
+	else if (data >  root->data) {
         root->right = deleteNode(root->right,data);
-    } else {
-        //found node
-        //delete
+    }
+
+	else {
         if (root->left == NULL || root->right == NULL) {
-            Node * tempNode = root->left ? root->left : root->right;
-            if ( tempNode == NULL) {
-                tempNode = root;
+            Node * temp = root->left ? root->left : root->right;
+            if (temp == NULL) {
+                temp = root;
                 root = NULL;
-            } else {
-                *root = *tempNode;
-                free(tempNode);
             }
-        } else {
+
+			else {
+                *root = *temp;
+                free(temp);
+			}
+        }
+
+		else {
 			Node * temp = root->left;
 			while(temp->right != NULL) {
         		temp = temp->right;
@@ -215,27 +218,29 @@ Node * deleteNode(struct Node *root,int data) {
         }
     }
 
-    if (root == NULL) return NULL;
+    if (root == NULL) {
+		return NULL;
+	}
 
     root->height = 1 + max(height(root->left),height(root->right));
     int balance = getBalance(root);
 
-    //left rotate
+    //left
     if (balance > 1 && getBalance(root->left) >= 0) {
         return rightRotate(root);
     }
 
-    if (balance > 1 && getBalance(root->left) < 0) {
+    else if (balance > 1 && getBalance(root->left) < 0) {
         root->left = leftRotate(root->left);
         return rightRotate(root);
     }
 
-    //rigth rotate
-    if (balance < -1 && getBalance(root->right) <= 0) {
+    //right
+    else if (balance < -1 && getBalance(root->right) <= 0) {
         return leftRotate(root);
     }
 
-    if (balance < -1 && getBalance(root->right) > 0 ) {
+    else if (balance < -1 && getBalance(root->right) > 0 ) {
         root->right = rightRotate(root->right);
         return leftRotate(root);
     }
